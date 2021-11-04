7a3fbfcb3eccf442dadd64760595c4c5b9b2c346
Bansharee Ireen <f004qs3@plank.thayer.dartmouth.edu>
2021-11-04 09:40:52 -0400

    starting creater

---
+++ Sudoku/sudoku.c
@@ -0,0 +1,20 @@
+/*
+ * sudoku.c - can either create or solve sudoku boards
+ *
+ * usage: ./sudoku mode difficulty
+ * 
+ * Bansharee Ireen, Ethan Chen, Jeffrey Jiang
+ * Nov 4, 2021
+ * 
+ * CS50, Fall 2021
+ */
+
+void validateParam(char* mode, char* difficulty);
+
+void validateParam(char* mode, char* difficulty) {
+
+}
+
+int main(const int argc, const char* argv[]) {
+
+}
