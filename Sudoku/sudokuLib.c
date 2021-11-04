c3e485cfc4e049c4cfa19be6e0173fb26c2495ec
Ethan L. Chen <f004h1g@plank.thayer.dartmouth.edu>
2021-11-03 20:47:20 -0400

    index.h

---
+++ Sudoku/sudokuLib.c
@@ -0,0 +1,35 @@
+/* 
+ * sudokuLib.h - header file for 'sudokuLib' file in 'Sudoku' module
+ *
+ * provides a set of functions for a sudoku table, that can print, swap rows, or.....
+ * 
+ * Ethan Chen, Jeffrey Jiang, Bansharee Ireen, November 2021
+ */
+
+#ifndef __SUDOKULIB
+#define __SUDOKULIB
+
+#include <stdbool.h>
+
+/******************* functions *******************/
+
+/******************* printTable() ******************/
+/*
+ * Function used to create a new index struct
+ * It creates a hashtable with the given size
+*/
+void printTable(int** table, bool style);
+
+/******************* swapRow() ******************/
+/*
+ * swaps two rows in a sudoku table
+*/
+void swapRow(int** table, int row1, int row2);
+
+/******************* swapColumn() ******************/
+/*
+ * Swaps two columns in a sudokou table
+*/
+void swapColumn(int** table, int col1, int col2);
+
+#endif
