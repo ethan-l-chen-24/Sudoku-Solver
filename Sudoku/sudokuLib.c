d6391c0291787ea3b4212fbdf77eeb0fdf41f1d8
Ethan L. Chen <f004h1g@plank.thayer.dartmouth.edu>
2021-11-03 23:53:08 -0400

    drafted sudokuLib

--- Sudoku/sudokuLib.c
+++ Sudoku/sudokuLib.c
@@ -10,23 +10,73 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/***************** helper functions *******************/
+
+static void printRowBar(void);
+
 /******************* printTable() ******************/
 /* see sudokuLib.h for more information */
-void printTable(int** table, bool style)
+void printTable(int** table, int dimension, bool style)
 {
+    if(table == NULL) return;
 
+    if(style) {
+        for(int i = 0; i < dimension; i ++) {
+            for(int j = 0; j < dimension; j++) {
+                printf("%s ", table[i][j]);
+            }
+            printf("\n");
+        }
+    } else {
+
+        int rowCount = 0;
+        int columnCount = 0;
+
+        printRowBar();
+        printf("|");
+        for(int i = 0; i < dimension; i ++) {
+            for(int j = 0; j < dimension; j++) {
+                columnCount++;
+                printf("%s ", table[i][j]);
+                if (columnCount % 3 == 0) {
+                    printf("|");
+                }
+            }
+            rowCount++;
+            if(rowCount % 3 == 0) {
+                printRowBar();
+            }
+            printf("\n|");
+        }
+    }
+}
+
+/******************* swapRow() ******************/
+/* print a long row bar _______________________ */
+static void printRowBar(void) {
+    printf("___________\n");
 }
 
 /******************* swapRow() ******************/
 /* see sudokuLib.h for more information */
-void swapRow(int** table, int row1, int row2)
+void swapRow(int** table, int dimension, int row1, int row2)
 {
-    
+    int save;
+    for(int i = 0; i < dimension; i++) {
+        save = table[row1][i];
+        table[row1][i] = table[row2][i];
+        table[row2][i] = save;
+    }
 }
 
 /******************* swapColumn() ******************/
 /* see sudokuLib.h for more information */
-void swapColumn(int** table, int col1, int col2) 
+void swapColumn(int** table, int dimension, int col1, int col2) 
 {
-
+    int save;
+    for(int i = 0; i < dimension; i++) {
+        save = table[i][col1];
+        table[i][col1] = table[i][col2];
+        table[i][col2] = save;
+    }
 }
--- Sudoku/sudokuLib.h
+++ Sudoku/sudokuLib.h
@@ -18,18 +18,18 @@
  * Function used to create a new index struct
  * It creates a hashtable with the given size
 */
-void printTable(int** table, bool style);
+void printTable(int** table, int dimension, bool style);
 
 /******************* swapRow() ******************/
 /*
  * swaps two rows in a sudoku table
 */
-void swapRow(int** table, int row1, int row2);
+void swapRow(int** table, int dimension, int row1, int row2);
 
 /******************* swapColumn() ******************/
 /*
  * Swaps two columns in a sudokou table
 */
-void swapColumn(int** table, int col1, int col2);
+void swapColumn(int** table, int dimension, int col1, int col2);
 
 #endif
