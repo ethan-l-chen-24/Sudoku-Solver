8f15f7792315830201e7ed0abd87cbd776ce5b8b
Ethan L. Chen <f004h1g@plank.thayer.dartmouth.edu>
2021-11-04 17:15:44 -0400

    finished draft of randomized swapping

--- Sudoku/creator.c
+++ Sudoku/creator.c
@@ -53,12 +53,40 @@ int** generateTable() {
 }
 
 /******************* randomize() ******************/
-/* see creator.h for more information */
-
+/* 
+ * takes a table and randomly swaps rows and columns 
+ * only swaps rows and columns within the same "group"
+ * groups are represented by indices 0-2, 3-5, and 6-8 for both rows and columns
+*/
 void randomize(int** table) {
     
-    for(int row = 0; row < 9; row++) {
-        for(int col = 0; col < 9; col++) {
+    // loop through every group of 3
+    for(int rowGroup = 0; rowGroup < 9; rowGroup += 3) {
+        for(int colGroup = 0; colGroup < 9; colGroup += 3) {
+            
+            // generate two unique numbers between 0 and 2
+            int randCol1 = rand() % 3;
+            int randCol2 = randCol1;
+            while(randCol1 == randCol2) {
+                randCol2 = rand() % 3;
+            }
+            
+            // add that value to whatever group we are currently looking at
+            randCol1 += colGroup;
+            randCol2 += colGroup;
+
+            // repeate above for columns
+            int randRow1 = rand() % 3;
+            int randRow2 = randRow1;
+            while(randRow1 == randRow2) {
+                randRow2 = rand() % 3;
+            }
+            randRow1 += rowGroup;
+            randRow2 += rowGroup;
+
+            // swap the row and columns randomly generated
+            swapColumn(table, 9, randCol1, randCol2);
+            swapRow(table, 9, randRow1, randRow2);
             
         }
     }
