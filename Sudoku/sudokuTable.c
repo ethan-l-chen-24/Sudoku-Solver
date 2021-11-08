8af109b732c0eb6f887d6f220ee9dc460e4bc52d
Jeffrey Jiang <f005py5@plank.thayer.dartmouth.edu>
2021-11-07 19:07:18 -0500

    fixed bug with sudokuTable_table, should be returning int**

--- Sudoku/sudokuTable.c
+++ Sudoku/sudokuTable.c
@@ -55,7 +55,7 @@ sudokuTable_t* sudokuTable_new(int dimension) {
 
 /******************* sudokuTable_board ******************/
 /* see sudokuTable.h for more information */
-sudokuTable_t* sudokuTable_board(sudokuTable_t* sudoku) {
+int** sudokuTable_board(sudokuTable_t* sudoku) {
     if(sudoku == NULL) return NULL;
     else return sudoku->table;
 }
