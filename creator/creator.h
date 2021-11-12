97240c2b097fa974126442c5b1299ab5ae8f9e36
[Jeffrey Jiang] <f005py5@plank.thayer.dartmouth.edu>
2021-11-11 23:25:24 -0500

    cleaned up comments and added more comments

--- creator/creator.h
+++ creator/creator.h
@@ -63,8 +63,44 @@ sudokuTable_t* generate(int numFilled, int dimension);
 bool checkUniqueness(sudokuTable_t* sudoku, int dimension);
 
 
+
+/******************* randomize ******************/
+/*
+ * takes in array and randomizes the order of the array 
+ *
+ * Caller provides:
+ *  array of ints and dimension or size of the array
+ *  
+ * 
+ * We return:
+ *  nothing
+ *  
+ * 
+ * We guarantee:
+ *  the array will be shuffled 
+ */
 void randomize(int* arr, int dimension);
 
+
+
+/******************* backtrackRand ******************/
+/*
+ * attempts solve a sudoku board passed to it in place (does not use extra memory). However, this differs
+ * from our backtrack algorithm in solver.c because it tries the candidates in a cell in random order. 
+ *
+ * Caller provides:
+ *  nothing, this will be implicity called in solveSudoku
+ *  
+ * 
+ * We return:
+ *  boolean return type which does hold any significance, just for backtracking recursion. 
+ * 
+ * We guarantee:
+ *  sudoku board will be solved if valid
+ * 
+ * Caller is responsible for later freeing the memory allocated,
+ * typically by calling sudokuTable_delete
+ */
 bool backtrackRand(int** board, int r, int c,  bool** row, bool** col, bool*** boxes, int dimension, int* arr);
 
 #endif
